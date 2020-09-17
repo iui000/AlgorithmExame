530a07ef94e919b50517a08abde875747b401bbb
GinkgoStack <liyalin.iui@gmail.com>
2020-09-17 21:04:40 +0800

    yalin

--- src/Ya/J/Main.java
+++ src/Ya/J/Main.java
@@ -4,24 +4,45 @@ import java.util.Scanner;
 
 public class Main {
 
-//    public static int fun(String[] args) {
+
+
+    public static int fun(int n,int m) {
+        int ans = 0;
+        double tar = m*100;
+
+        int left = 1;
+        int right = 20;
+
+
+        while (left < right){
+            int mid = (left + right)/2;
+            double s = 2*( Math.pow(2,mid) -1);
+            double x = s/Math.pow(2,mid);
+
+            if(n * x > tar){
+                right = mid-1;
+            }
+
+            if(n * x < tar){
+                left = mid+1;
+            }
+
+            if(n * x == tar){
+                return left;
+            }
+        }
+
+
+        return left;
+    }
 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        int ans = 0;
-        double tar = m*100;
-        double tmp = 0.0;
-        int i = 1;
-        while (tar > 0.0){
-            tmp += Math.pow(2,i);
-            double x = n - n/tmp;
-            tar -= x;
-            ans++;
-            i++;
-        }
-        System.out.println(ans);
+
+
+        System.out.println(fun(n,m));
     }
 }
